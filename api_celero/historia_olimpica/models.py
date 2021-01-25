7786b80e1b13b87b0592fbf4848fa1ba44a55327
Danilo Costa <danilo.barbosa@fapce.edu.br>
2021-01-25 20:28:03 -0300

    Model AthleteEvents criado.

--- api_celero/historia_olimpica/models.py
+++ api_celero/historia_olimpica/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class AthleteEvents(models.Model):
+    SEXO_CHOICES = (
+        ("F", "Feminino"),
+        ("M", "Masculino"),
+        ("N", "Nenhuma das opções")
+    )
+
+    name = models.CharField(max_length=100, null=False, blank=False)
+    sex = models.CharField(max_length=1, choices=SEXO_CHOICES, blank=False, null=False)
+    age = models.IntegerField(null=False, blank=False)
+    height = models.FloatField(null=False, blank=False)
+    weight = models.FloatField(null=False, blank=False)
+    team = models.CharField(max_length=100, null=False, blank=False)
+    noc = models.CharField(max_length=3, null=False, blank=False)
+    games = models.CharField(max_length=100, null=False, blank=False)
+    year = models.IntegerField(null=False, blank=False)
+    season = models.CharField(max_length=100, null=False, blank=False)
+    city = models.CharField(max_length=100, null=False, blank=False)
+    sport = models.CharField(max_length=100, null=False, blank=False)
+    event = models.CharField(max_length=100, null=False, blank=False)
+    medal = models.CharField(max_length=15, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
+
+
+
+
