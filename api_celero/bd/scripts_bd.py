e339259270cfffb405003bdd2edbe70b5819c333
Danilo Costa <danilo.barbosa@fapce.edu.br>
2021-01-27 11:34:00 -0300

    Scripts para popular BD finalizados. Modelagem da tabela AthleteEvents corrigida para abrigar os dados do CSV, as correções foram: aumento do tamanho das colunas e flexibilização na entrada do dados, permitindo itens null=True e blanck=True.

--- api_celero/bd/scripts_bd.py
+++ api_celero/bd/scripts_bd.py
@@ -9,7 +9,6 @@ class Config:
                 "password": "12345",
                 "host": "localhost",
                 "database": "api_desafio_celero"
-
             }
         }
 
@@ -50,6 +49,10 @@ class Connection(Config):
 
     def execute(self, sql, params=None):
         self.cursor.execute(sql, params or ())
+
+
+    def query(self, sql, params=None):
+        self.cursor.execute(sql, params or ())
         return self.fetchall()
 
 
@@ -66,6 +69,9 @@ class NocRegions(Connection):
         except Exception as e:
             print(f'Erro ao inserir: {e}')
 
+    def select(self, *args):
+        pass
+
 class AthleteEvents(Connection):
     def __init__(self):
         Connection.__init__(self)
@@ -75,14 +81,17 @@ class AthleteEvents(Connection):
         try:
             sql = '''
             INSERT INTO 
-            historia_olimpica_athleteevents (noc, region, notes) 
-            VALUES (%s, %s, %s) 
+            historia_olimpica_athleteevents (name, sex, age, height, weight, team, noc_id, games, year, season, 
+            city, sport, event, medal) 
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) 
             RETURNING id;
             '''
             self.execute(sql, args)
             self.commit()
         except Exception as e:
             print(f'Erro ao inserir: {e}')
+
+### Scripts para a execução da inserção dos itens no BD
 
 def inserir_dados_na_tabela_noc_regions(df):
     noc_regions = NocRegions()
@@ -95,20 +104,42 @@ def inserir_dados_na_tabela_noc_regions(df):
         noc_regions.insert(noc, region, notes)
 
 
+def selecionar_id_na_tabela_noc_regions(noc: str):
+    noc_regions = NocRegions()
+    query_set = noc_regions.query(f"SELECT id FROM historia_olimpica_nocregions WHERE noc LIKE '{noc}'")
+    return query_set[0][0]
+
+
 def inserir_dados_na_tabela_athlete_events(df):
     athlete_events = AthleteEvents()
 
-    for i in df[:][1:].itertuples():
-        noc = i[1]
-        region = i[2]
-        notes = i[3]
+    for index, i in enumerate(df[:][154:].itertuples()):
+        name = i[2]
+        sex = i[3]
+        age = i[4]
+        height = i[5]
+        weight = i[6]
+        team = i[7]
+        noc = selecionar_id_na_tabela_noc_regions(i[8])
+        games = i[9]
+        year = i[10]
+        season = i[11]
+        city = i[12]
+        sport = i[13]
+        event = i[14]
+        medal = i[15]
 
-        athlete_events.insert(noc, region, notes)
+        try:
+            athlete_events.insert(name, sex, age, height, weight, team, noc, games, year, season,
+            city, sport, event, medal)
+        except Exception as e:
+            print(f'Erro ao inserir item {index}, veja: {e}')
+
 
 if __name__ == '__main__':
     df_noc_regions = carregar_csv('noc_regions.csv')
     inserir_dados_na_tabela_noc_regions(df_noc_regions)
 
     df_athlete_events = carregar_csv('athlete_events.csv')
-
+    inserir_dados_na_tabela_athlete_events(df_athlete_events)
 
