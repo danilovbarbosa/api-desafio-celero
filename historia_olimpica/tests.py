37196a7f524893fe8cb99abc017e6d9da90e0e03
Danilo Costa <danilo.barbosa@fapce.edu.br>
2021-01-27 20:36:35 -0300

    Classe para teste de registros foi criada. Primeiro teste para registro de noc_regions já operando.

--- historia_olimpica/tests.py
+++ historia_olimpica/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+import json
+from profile import Profile
 
-# Create your tests here.
+from django.contrib.auth.models import User
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APITestCase
+
+from historia_olimpica.models import NocRegions, AthleteEvents
+from historia_olimpica.serializers import NocRegionsSerializer, AthleteEventsSerializer
+
+class RegistrosTestCase(APITestCase):
+    def test_registro_de_noc_regions_deve_retornar_status_200(self):
+        data = {
+            "noc": "teste",
+            "region": "teste",
+            "notes": "teste"
+        }
+        response = self.client.post('/historia_olimpica/noc_regions/', data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
